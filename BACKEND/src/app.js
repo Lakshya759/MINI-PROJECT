fb9b0bb1a96a1069ca2b19f3aa500bcabd5d523a
Lakshya759 <lakshyasri759@gmail.com>
2025-11-02 19:53:37 +0530

    moved the backend in the backend folder

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -11,10 +11,12 @@ app.use(cookieParser())
 //routes
 import userRouter from "./routes/user.routes.js"
 import postRouter from "./routes/post.routes.js"
+import answerRouter from "./routes/answer.routes.js"
 
 //routes declaration
 
 app.use("/api/v1/users",userRouter)
 app.use("/api/v1/posts",postRouter)
+app.use("/api/v1/answer",answerRouter)
 
 export {app}
